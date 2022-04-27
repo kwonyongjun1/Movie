b9d50edf056d4c5c55eebada3cb9b51f748fec40
YongJun Kwon <dydwns18011@gmail.com>
2022-04-27 23:34:28 +0900

    화면별 컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-import {useEffect, useState} from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    useEffect(() =>{
-        axios.get('https://yts.mx/api/v2/list_movies.json?minimum_rating=9')
-            .then((Response)=>{
-              setMovies(Response.data.data.movies);
-              setLoading(false);
-            }).catch((Error)=>{
-                console.log(Error);
-        });
-    },[]);
-    console.log(movies);
-  return (
-    <div>
-        {
-            loading ?
-                (<h1>Loading...</h1>
-                ):(
-                    <div> {movies.map(movie =>
-                        <div key = {movie.id}>
-                            <img src = {movie.medium_cover_image} />
-                            <h2>{movie.title}</h2>
-                            <p>{movie.summary}</p>
-                            <ul>
-                            {movie.genres.map((g) => (
-                                  <li key={g}>{g}</li>
-                             ))}
-                            </ul>
-                        </div>
-                    )}
-                    </div>
-                )}
-    </div>
-  );
+   return(
+       <Router>
+           <Routes>
+               <Route path = '/movie' element={<Detail/>} />
+               <Route path = '/' element={<Home/>} />
+           </Routes>
+       </Router>
+   );
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,38 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+
+function Home(){
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    useEffect(() =>{
+        axios.get('https://yts.mx/api/v2/list_movies.json?minimum_rating=9')
+            .then((Response)=>{
+                setMovies(Response.data.data.movies);
+                setLoading(false);
+            }).catch((Error)=>{
+            console.log(Error);
+        });
+    },[]);
+    console.log(movies);
+    return (
+        <div>
+            {
+                loading ?
+                    (<h1>Loading...</h1>
+                    ):(
+                        <div> {movies.map((movie) => (
+                            <Movie
+                                key = {movie.id}
+                                coverImg = {movie.medium_cover_image}
+                                title = {movie.title}
+                                summary = {movie.summary}
+                                genres = {movie.genres}
+                            />
+                        ))}
+                        </div>
+                    )}
+        </div>
+    );
+}
+export default Home;
