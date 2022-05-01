6f8036eff028e21e188f9a441425790c31ac6f1a
YongJun Kwon <dydwns18011@gmail.com>
2022-05-01 19:57:11 +0900

    선택된 영화 상세보기 데이터 보여주기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,45 @@
 import { useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function Detail(){
-    const x = useParams()
-    console.log(x);
-    return <h1>Detail</h1>
+    const [loading,setLoading] = useState(true);
+    const [movie,setMovie] = useState([]);
+    const { id }= useParams();
+    const getMovie = async () =>{
+       await (
+          await axios.get(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+              .then((Response)=>{
+                  console.log(Response.data.data.movie);
+                  setLoading(false);
+                  setMovie(Response.data.data.movie);
+              }).catch((Error)=>{
+                  console.log(Error);
+              })
+      );
+    };
+    useEffect(()=>{
+        getMovie();
+    },[]);
+
+    return (
+        <div>
+            {
+                loading ?
+                    (
+                        <h1>Loading....</h1>
+                    ):(
+                        <div>
+                            <h1>{movie.id}</h1>
+                            <h1>{movie.title}</h1>
+                            <h1>{movie.date_uploaded}</h1>
+                            <h1></h1>
+                        </div>
+                    )
+            }
+
+        </div>
+        );
 }
 
 export default Detail;
